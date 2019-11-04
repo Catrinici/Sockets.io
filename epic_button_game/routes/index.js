9eb2f4e58901c0c60f3d95e442fd90ad58073838
Catrinici <catrinici.antonina@gmail.com>
2019-11-04 09:58:28 -0800

    finished the assignement

--- epic_button_game/routes/index.js
+++ epic_button_game/routes/index.js
@@ -1,25 +1,27 @@
 module.exports = function Route(app, server) {
-//this gets the socket.io module
-var io = require('socket.io').listen(server)
+    //this gets the socket.io module
+    var io = require('socket.io').listen(server);
     //root route to render the index.ejs view
-app.get('/', function(req, res) {
-        res.render('index')
-    })
+
     //listen to connection even from the client side
-io.sockets.on('connection', function(socket) {
-    //server listens tp "posting_form" event
-    var counter = 0;
-    $('#button').click(function() {
-        counter++;
+    io.sockets.on('connection', function(socket) {
+        //server listens tp "posting_form" event
+        var count = 0;
+        socket.on('button_click', function(data) {
+            count++;
+            console.log('counter: ', count);
+            socket.emit('update_count', { count: count });
+        });
+        socket.on('reset', function(data) {
+            count = 0;
+            console.log('counter', count);
+            socket.emit('update_count', { count: count });
+        });
 
-
-    })
-
-
-    //will emit the data to the c lient
-    socket.emit('counting', { response: data });
-
-
-})
-})
-}
+        //will emit the data to the c lient
+        // socket.emit('count', { response: data });
+    });
+    app.get('/', function(req, res) {
+        res.render('index');
+    });
+};
